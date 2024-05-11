8fa3babd49230f90ae400eace59b3814026a9c9d
rnomura <rnomura@student.42tokyo.jp>
2024-05-11 23:02:27 +0900

    final commit

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: rnomura <rnomura@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 17:31:46 by rnomura           #+#    #+#             */
-/*   Updated: 2024/05/06 22:41:44 by rnomura          ###   ########.fr       */
+/*   Updated: 2024/05/11 22:55:49 by rnomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (count && size && count > SIZE_MAX / size)
+		return (0);
 	ptr = (void *)malloc(count * size);
 	if (ptr != NULL)
 	{
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rnomura <rnomura@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 06:19:00 by rnomura           #+#    #+#             */
-/*   Updated: 2024/05/11 22:11:50 by rnomura          ###   ########.fr       */
+/*   Updated: 2024/05/11 22:50:56 by rnomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *tmp;
-	//t_list *memo;
-	t_list *new_node;
-	void *content;
+	t_list	*tmp;
+	t_list	*new_node;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
@@ -26,10 +25,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	{
 		content = f(lst->content);
 		tmp = ft_lstnew(content);
-		//memo = lst->next;
-		//ft_lstdelone(lst,del);
+		if (!tmp)
+		{
+			ft_lstclear(&new_node, del);
+			del(content);
+			return (NULL);
+		}
 		ft_lstadd_back(&new_node, tmp);
-		//lst = memo;
 		lst = lst->next;
 	}
 	return (new_node);
