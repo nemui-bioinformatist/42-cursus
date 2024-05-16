e72fdf617eb4bc227211939b430e82f8252fac94
rnomura <rnomura@student.42tokyo.jp>
2024-05-16 23:37:51 +0900

    ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rnomura <rnomura@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 11:12:09 by rnomura           #+#    #+#             */
-/*   Updated: 2024/05/04 11:58:45 by rnomura          ###   ########.fr       */
+/*   Updated: 2024/05/16 23:28:39 by rnomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	src_len;
 	size_t	i;
 
-	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
+	if (!dst && dstsize == 0)
+		return(dstsize + src_len);
+	dst_len = ft_strlen(dst);
 	i = 0;
-	if (!dst && !dstsize)
-		return (dstsize + src_len);
 	if (dst_len >= dstsize)
 		return (dstsize + src_len);
 	while (i < dstsize - dst_len - 1 && src[i])
